b301fc71e566c0a1b03764892a1b0c374ba4f349
Fardin Haque <fardinhaque60@gmail.com>
2025-01-26 12:49:41 -0800

    clean admin page so delete is on own page

--- app/pages/admin/page.tsx
+++ app/pages/admin/page.tsx
@@ -1,13 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
-import { Blog } from "@interfaces/Blog";
 
 const Admin = () => {
   const router = useRouter();
-  const [blogs, setBlogs] = useState<Blog[]>([]);
 
   useEffect(() => {
     const checkAuthentication = async () => {
@@ -25,46 +22,9 @@ const Admin = () => {
         router.push("/pages/login");
       }
     };
-    const fetchBlogs = async () => {
-      try {
-        const res = await fetch("/api/admin");
-
-        if (res.ok) {
-          const data = (await res.json()).data;
-          setBlogs(data);
-        } else {
-          console.error("Failed to fetch blogs");
-        }
-      } catch (error) {
-        console.error("Error fetching blogs:", error);
-      }
-    };
 
     checkAuthentication();
-    fetchBlogs();
   }, [router]);
-
-  const handleArticleClick = (id: string) => {
-    router.push(`/pages/articleView/${id}`);
-  };
-
-  // TODO pass index so remove it from blog list
-  const handleDelete = async (index: number) => {
-    try {
-      const blogId = blogs[index]._id;
-      await fetch("/api/admin", {
-        method: "DELETE",
-        body: JSON.stringify({ id: blogId }), // Send blog ID in the body
-        // TODO add body with blog id to delete
-      });
-
-      alert("Article deleted");
-      setBlogs((prevBlogs) => prevBlogs.filter((_, i) => i !== index));
-    } catch (error) {
-      console.error("Error deleting article:", error);
-      alert("Error deleting article:");
-    }
-  }
 
   const handleLogout = async () => {
     try {
@@ -98,30 +58,6 @@ const Admin = () => {
         <Link href="/pages/delete">
           <button>Delete page</button>
         </Link>
-      </div>
-      <div> {/* TODO Sarvy update formating */}
-        {blogs.map((blog: Blog, index) => (
-          <div key={blog._id}>
-            <div
-              onClick={() => handleArticleClick(blog._id)}
-              style={{ cursor: "pointer" }}
-            >
-              <h3>{blog.title}</h3>
-              <p className="">
-                {blog.author} - {new Date(blog.date).toLocaleDateString()}
-              </p>
-              <Image
-                src={`/api/image/${blog.image}`}
-                alt={blog.title}
-                className=""
-                width={300}
-                height={200}
-              />  
-            </div>
-            <button onClick={() => handleDelete(index)}>Delete</button>
-            <p> ----------------- </p>
-          </div>
-        ))}
       </div>
     </div>
   );
--- app/pages/delete/page.tsx
+++ app/pages/delete/page.tsx
@@ -1,75 +1,73 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import Image from "next/image";
 import { Blog } from "@interfaces/Blog";
 
-const deleter = () => {
+const Delete = () => {
+  const router = useRouter();
+  const [blogs, setBlogs] = useState<Blog[]>([]);
 
-    const router = useRouter();
-    const [blogs, setBlogs] = useState<Blog[]>([]);
-
-    useEffect(() => {
-        const checkAuthentication = async () => {
-          try {
-            const res = await fetch("/api/login", {
-              method: "GET",
-              credentials: "include", // Include cookies in the request
-            });
-    
-            if (!res.ok) { // if cookie is not present, redirect to login page
-              router.push("/pages/login");
-            }
-          } catch (error) {
-            console.error("Error checking authentication:", error);
-            router.push("/pages/login");
-          }
-        };
-
-        const fetchBlogs = async () => {
-            try {
-              const res = await fetch("/api/admin");
-      
-              if (res.ok) {
-                const data = (await res.json()).data;
-                setBlogs(data);
-              } else {
-                console.error("Failed to fetch blogs");
-              }
-            } catch (error) {
-              console.error("Error fetching blogs:", error);
-            }
-          };
-          checkAuthentication();
-          fetchBlogs();
-        }, [router]);
-
-    const handleArticleClick = (id: string) => {
-        router.push(`/pages/articleView/${id}`);
-      };
-
-  
-    const handleDelete = async (index: number) => {
-        try {
-        const blogId = blogs[index]._id;
-        await fetch("/api/admin", {
-            method: "DELETE",
-            body: JSON.stringify({ id: blogId }), // Send blog ID in the body
-            // TODO add body with blog id to delete
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const res = await fetch("/api/login", {
+          method: "GET",
+          credentials: "include", // Include cookies in the request
         });
 
-        alert("Article deleted");
-        setBlogs((prevBlogs) => prevBlogs.filter((_, i) => i !== index));
-        } catch (error) {
-        console.error("Error deleting article:", error);
-        alert("Error deleting article:");
+        if (!res.ok) { // if cookie is not present, redirect to login page
+          router.push("/pages/login");
         }
-    }
+      } catch (error) {
+        console.error("Error checking authentication:", error);
+        router.push("/pages/login");
+      }
+    };
 
+    const fetchBlogs = async () => {
+        try {
+          const res = await fetch("/api/admin");
+  
+          if (res.ok) {
+            const data = (await res.json()).data;
+            setBlogs(data);
+          } else {
+            console.error("Failed to fetch blogs");
+          }
+        } catch (error) {
+          console.error("Error fetching blogs:", error);
+        }
+      };
+      checkAuthentication();
+      fetchBlogs();
+    }, [router]);
+
+  const handleArticleClick = (id: string) => {
+    router.push(`/pages/articleView/${id}`);
+  };
+
+  const handleDelete = async (index: number) => {
+    try {
+    const blogId = blogs[index]._id;
+    await fetch("/api/admin", {
+        method: "DELETE",
+        body: JSON.stringify({ id: blogId }), // Send blog ID in the body
+    });
+
+    alert("Article deleted");
+    setBlogs((prevBlogs) => prevBlogs.filter((_, i) => i !== index));
+    } catch (error) {
+      console.error("Error deleting article:", error);
+      alert("Error deleting article:");
+    }
+  }
 
   return(
-    <div> {/* TODO Sarvy update formating */}
+    <div>
+      <h1>Modify Blogs</h1>
+      <h3>Select a Blog below to modify it, or select delete to remove it.</h3>
+      <div> {/* TODO Sarvy update formating */}
         {blogs.map((blog: Blog, index) => (
           <div key={blog._id}>
             <div
@@ -92,10 +90,9 @@ const deleter = () => {
             <p> ----------------- </p>
           </div>
         ))}
+      </div>
     </div>
-    
   );
-
 };
 
-export default deleter;
+export default Delete;
