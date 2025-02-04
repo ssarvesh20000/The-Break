9d9fe2b1f01991cdebe1fc959c47f61d964a07b8
Sarvesh Shanmugam <ssarvesh200000@gmail.com>
2025-02-04 16:16:18 -0700

    editpage for media added, and toggle in modify page

---
+++ app/pages/mediaChange / [id]/page.tsx
@@ -0,0 +1,213 @@
+'use client';
+import React, { useState, useEffect } from "react";
+import "@styles/write.css";
+import { useRouter, useParams } from "next/navigation";
+import { Media } from "@interfaces/Media";
+
+
+
+const MediaEdit = () => {
+
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isFormDirty, setIsFormDirty] = useState(false);
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+  const [media, setMedia] = useState<Media | null>(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchArticle = async () => {
+      try {
+        console.log("Fetching article with ID:", id);
+        const response = await fetch(`/api/media/${id}`);
+        const data = await response.json();
+        setMedia(data.data);
+
+        const media = data.data;
+        if (media) {
+          // prefill all fields in form
+          setTitle(media?.title || ""); 
+          setAuthor(media?.author || ""); 
+          setYoutubeLink(media?.youtubeLink || "");
+          setDescription(media?.description || ""); 
+        }
+        
+      } catch (error) {
+        console.error("Error fetching article:", error);
+      }
+    };
+
+    const checkAuthentication = async () => {
+      try {
+        const res = await fetch("/api/login", {
+          method: "GET",
+          credentials: "include",
+        });
+        if (!res.ok) router.push("/pages/login");
+      } catch (error) {
+        console.error("Error checking authentication:", error);
+        router.push("/pages/login");
+      }
+    };
+
+    checkAuthentication();
+    fetchArticle();
+
+    // handle reload and tab exit protection
+    function beforeUnload(e: BeforeUnloadEvent) {
+      if (isFormDirty) {
+        e.preventDefault();
+        confirm("Are you sure you want to leave? Your changes will be discarded.")
+      }
+    }
+  
+    window.addEventListener('beforeunload', beforeUnload);
+  
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnload);
+    };
+  }, [router]);
+
+  const clearForm = () => {
+    setTitle("");
+    setAuthor("");
+    setDescription("");
+    setYoutubeLink("");
+    setIsFormDirty(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    
+    const formData = new FormData();
+    formData.append("id", media?._id || "");
+    formData.append("title", title);
+    formData.append("author", author);
+    formData.append("youtubeLink", youtubeLink);
+    formData.append("description", description);
+
+    try {
+      const res = await fetch("/api/admin", {
+        method: "PUT",
+        body: formData,
+      });
+
+      if (res.ok) {
+        alert("Media updated successfully");
+        clearForm();
+        router.push("/pages/admin");
+      } else {
+        alert("Failed to update media");
+      }
+    } catch (error) {
+      console.error("Error creating Media:", error);
+      alert("Error updating media");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleNavigation = () => {
+    if (isFormDirty) {
+      // Show confirmation dialog
+      const confirmed = window.confirm("Your changes will be discarded. Are you sure you want to leave?");
+      if (confirmed) {
+        // Navigate to the target page
+        router.push("/pages/modify");
+      }
+    } else {
+      // No unsaved changes, just navigate
+      router.push("/pages/modify");
+    }
+  };
+
+  const handleDelete = async () => {
+    if (!confirm("Are you sure you want to permanently delete this article?")) {
+      return;
+    }
+    try {
+      const res = await fetch("/api/admin", {
+        method: "DELETE",
+        body: JSON.stringify({ id: media?._id }),
+      });
+      if (res.ok) {
+        alert("Article deleted");
+        router.push("/");
+      } else {
+        alert("Failed to delete article");
+      }
+    } catch (error) {
+      console.error("Error deleting article:", error);
+      alert("Error deleting article");
+    }
+  };
+  
+  return (
+    <div className="container">
+      <button onClick={handleNavigation} className="return-button"> ← Return to Modify Page </button>
+      <div className="update-blog-header">
+        <h1>Update Media</h1>
+        <h3>Change fields below to update the Media. Leave this page to discard changes.</h3>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <h1>Modify Title</h1>
+        {/* TODO double check if this is good */}
+        <input 
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => { setTitle(e.target.value); setIsFormDirty(true); }} 
+          required
+        />
+        
+        <h1>Modify Author</h1>
+        <input 
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => { setAuthor(e.target.value); setIsFormDirty(true); }}
+          required
+        />
+
+        <h1>Modify YouTube Link</h1>
+        <input 
+          type="text"
+          placeholder="YouTube Link"
+          value={youtubeLink}
+          onChange={(e) => { setYoutubeLink(e.target.value); setIsFormDirty(true); }}
+          required
+        />
+
+        <h1>Modify Media Description</h1>
+        <textarea 
+          placeholder="Description"
+          value={description}
+          onChange={(e) => { setDescription(e.target.value); setIsFormDirty(true); }}
+          required
+        />
+
+        <button 
+          type="submit" 
+          className="submit-button" 
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "Saving Changes..." : "Save Changes"}
+        </button>
+        <button type="button" 
+        className="delete-button" 
+        onClick={handleDelete}>Delete Article
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MediaEdit;
--- app/pages/modify/page.tsx
+++ app/pages/modify/page.tsx
@@ -3,13 +3,17 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { Blog } from "@interfaces/Blog";
+import { Media } from "@interfaces/Media";
 import "@styles/Modify.css";
 import Loading from "@components/Loading";
 
-const Delete = () => {
+//create toggle to show the media upload (youtube link) field
+const Modify = () => {
   const router = useRouter();
   const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [mediaUploads, setMediaUploads] = useState<Media[]>([]);
   const [loading, setLoading] = useState(false);
+  const [showMedia, setShowMedia] = useState(false);
 
   useEffect(() => {
     const checkAuthentication = async () => {
@@ -29,49 +33,75 @@ const Delete = () => {
     };
 
     const fetchBlogs = async () => {
-        try {
-          const res = await fetch("/api/admin");
-  
-          if (res.ok) {
-            const data = (await res.json()).data;
-            setBlogs(data);
-          } else {
-            console.error("Failed to fetch blogs");
-          }
-        } catch (error) {
-          console.error("Error fetching blogs:", error);
+      try {
+        const res = await fetch("/api/admin");
+
+        if (res.ok) {
+          const data = (await res.json()).data;
+          setBlogs(data);
+        } else {
+          console.error("Failed to fetch blogs");
         }
-      };
-      checkAuthentication();
-      fetchBlogs();
-    }, [router]);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+
+    const fetchMedia = async () => {
+      try {
+        const res = await fetch("/api/media");
+        if (res.ok) {
+          const data = (await res.json()).data;
+          setMediaUploads(data);
+        } else {
+          console.error("Failed to fetch media uploads");
+        }
+      } catch (error) {
+        console.error("Error fetching media uploads:", error);
+      }
+    };
+
+    checkAuthentication();
+    fetchBlogs();
+    fetchMedia();
+  }, [router]);
 
   const handleArticleClick = (id: string) => {
     router.push(`/pages/blogChange/${id}`);
   };
 
-  const handleDelete = async (index: number) => {
+  const handleMediaClick = (id: string) => {
+    router.push(`/pages/mediaChange/${id}`);
+  }
+
+  const handleDelete = async (id: string, type: "blog" | "media") => {
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete this ${type}? This action cannot be undone.`
+    );
+    if (!confirmDelete) return;
+
     try {
-      // give alert to confirm deletion, if user agrees continue with deletion, if not exit
-      if (!confirm("Are you sure you want to permanently delete this article?")) {
-        return;
-      }
       setLoading(true);
-      const blogId = blogs[index]._id;
-      await fetch("/api/admin", {
-          method: "DELETE",
-          body: JSON.stringify({ id: blogId }), // Send blog ID in the body
+
+      const endpoint = type === "blog" ? "/api/admin" : "/api/media";
+      await fetch(endpoint, {
+        method: "DELETE",
+        body: JSON.stringify({ id }),
       });
 
-      alert("Article deleted");
-      setBlogs((prevBlogs) => prevBlogs.filter((_, i) => i !== index));
+      alert(`${type.charAt(0).toUpperCase() + type.slice(1)} deleted.`);
+      if (type === "blog") {
+        setBlogs((prev) => prev.filter((blog) => blog._id !== id));
+      } else {
+        setMediaUploads((prev) => prev.filter((media) => media._id !== id));
+      }
     } catch (error) {
-      console.error("Error deleting article:", error);
-      alert("Error deleting article:");
+      console.error(`Error deleting ${type}:`, error);
+      alert(`Error deleting ${type}.`);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   if (loading) {  
     return <Loading />;
@@ -81,39 +111,89 @@ const Delete = () => {
     <div className="delete-container">
       <button onClick={() => { router.push("/pages/admin") }} className="return-button"> ← Return to Dashboard </button>
       <div className="header">
-        <h1>Modify Blogs</h1>
-        <h3>Select a Blog below to modify it, or select delete to remove it.</h3>
+        <h1>Modify {showMedia ? "Media" : "Blogs"}</h1>
+        <h3>Select an item below to modify it, or select delete to remove it.</h3>
       </div>
+
+      {/* Toggle Switch for Blogs/Media */}
+      <div className="toggle-container">
+        <label className="toggle-label">
+          <span>Show Blogs</span>
+          <input
+            type="checkbox"
+            className="toggle-checkbox"
+            checked={showMedia}
+            onChange={() => setShowMedia(!showMedia)}
+          />
+          <span className="toggle-slider"></span>
+          <span>Show Media Uploads</span>
+        </label>
+      </div>
+
       
-      <div className="blog-grid">
-        {blogs.map((blog: Blog, index) => (
-          <div key={blog._id} className="blog-card">
-            <div onClick={() => handleArticleClick(blog._id)}>
-              <Image
-                src={`/api/image/${blog.image}`}
-                alt={blog.title}
-                className="blog-image"
-                width={300}
-                height={200}
-              />
-              <div className="blog-content">
-                <h3 className="blog-title">{blog.title}</h3>
-                <p className="blog-meta">
-                  {blog.author} - {new Date(blog.date).toLocaleDateString()}
-                </p>
+      {showMedia ? (
+        // Display Media Uploads (YouTube Videos)
+        <div className="blog-grid">
+          {mediaUploads.map((media) => (
+            <div key={media._id} className="blog-card">
+              <div onClick={() => router.push(`/pages/mediaChange/${media._id}`)}>
+                <iframe
+                  width="300"
+                  height="200"
+                  src={`https://www.youtube.com/embed/${new URL(media.youtubeLink).searchParams.get("v")}`}
+                  title={media.title}
+                  //frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+                <div className="blog-content">
+                  <h3 className="blog-title">{media.title}</h3>
+                  <p className="blog-meta">
+                    {media.author} - {new Date(media.date).toLocaleDateString()}
+                  </p>
+                </div>
               </div>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(media._id, "media")}
+              >
+                Delete
+              </button>
             </div>
-            <button 
-              className="delete-button"
-              onClick={() => handleDelete(index)}
-            >
-              Delete
-            </button>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        // Display Blogs
+        <div className="blog-grid">
+          {blogs.map((blog) => (
+            <div key={blog._id} className="blog-card">
+              <div onClick={() => router.push(`/pages/blogChange/${blog._id}`)}>
+                <Image
+                  src={`/api/image/${blog.image}`}
+                  alt={blog.title}
+                  className="blog-image"
+                  width={300}
+                  height={200}
+                />
+                <div className="blog-content">
+                  <h3 className="blog-title">{blog.title}</h3>
+                  <p className="blog-meta">
+                    {blog.author} - {new Date(blog.date).toLocaleDateString()}
+                  </p>
+                </div>
+              </div>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(blog._id, "blog")}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
-export default Delete;
+export default Modify;
